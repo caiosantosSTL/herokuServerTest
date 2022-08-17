3022423f3af3489fc7839988d039fc1e5d435c0a
Caio Santos da Silva <vorreicaio@hotmail.com>
2022-08-16 19:15:16 -0500

    new update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res)=>{
-    res.send('This is a server')
+    res.send('This is a server!!!')
 })
 
 const PORT = process.env.PORT || 3400
